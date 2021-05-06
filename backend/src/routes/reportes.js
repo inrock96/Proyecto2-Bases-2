8a118b9b12846af336aebdaef223374b5318bff1
Inti <intirocks888@gmail.com>
2021-05-05 22:15:45 -0700

    backend terminado

--- backend/src/routes/reportes.js
+++ backend/src/routes/reportes.js
@@ -1,27 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const fs = require('fs')
-var cassandra = require('cassandra-driver');
-var client = new cassandra.Client({ contactPoints: ['192.168.1.20']});
+const cassandra = require('cassandra-driver');
+const client = new cassandra.Client({ contactPoints: ['localhost'],localDataCenter:'datacenter1',keyspace:'proyecto2'});
 
 
 router.post('/operacion/simple',(req,res)=>{
     let body = req.body
     let cuenta = body.cuenta
-    let query = `SELECT * FROM operacion_por_cuentahabiente WHERE cui1 = ?;`
-    client.execute(query,[cuenta]).then(result=>{
+    const query = `SELECT * FROM operacion_por_cuentahabiente WHERE cui1 = ${cuenta}`;
+    console.log(cuenta)
+    client.execute(query).then(result=>{
         res.send(result.rows)
     })
 })
 
-
-router.post('/debito',(req,res)=>{
-    let body = req.body
-    let inst = body.institucion
-    client.execute('SELECT * FROM proyecto2.debito_por_institucion WHERE institucion1 = ?',[inst]).then(result=>{
-        res.send(result.rows)
-    })
-})
 router.post('/credito',(req,res)=>{
     let body = req.body
     let inst = body.institucion
@@ -30,8 +22,10 @@ router.post('/credito',(req,res)=>{
     })
 })
 
-router.get('/instituciones',(req,res)=>{
-    client.execute('SELECT * FROM proyecto2.institucion').then(result=>{
+router.post('/debito',(req,res)=>{
+    let body = req.body
+    let inst = body.institucion
+    client.execute('SELECT * FROM proyecto2.debito_por_institucion WHERE institucion1 = ?',[inst]).then(result=>{
         res.send(result.rows)
     })
 })
@@ -41,12 +35,17 @@ router.get('/cuentahabientes',(req,res)=>{
         res.send(result.rows)
     })
 })
-
+router.get('/instituciones',(req,res)=>{
+    client.execute('SELECT * FROM proyecto2.institucion').then(result=>{
+        res.send(result.rows)
+    })
+})
 router.post('/operacion/mensual',(req,res)=>{
     let body = req.body
     let cuenta = body.cuenta
-    let mes = mes
-    let query = `SELECT * FROM operacion_por_cuentahabiente WHERE cui1 = ${cuenta} AND fecha >= '2020-0${mes}-01' AND fecha <='2020-0${mes}-30';`
+    let mes2 = body.mes
+    let mes = mes2++;
+    let query = `SELECT * FROM operacion_por_cuentahabiente WHERE cui1 = ${cuenta} AND fecha > '2020-0${mes}-01' AND fecha <'2020-0${mes2}-01';`
     client.execute(query).then(result=>{
         res.send(result.rows)
     })
